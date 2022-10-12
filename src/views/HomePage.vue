327d6bb6cfccf27c8b10d6149f4ac31c6014ac5b
LukasBudach <lukas.budach@student.hpi.de>
2022-10-12 17:24:29 +0200

    works

---
+++ src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1> Home Page </h1>
+
+  <button @click="generateQRCode"> Get QR Code </button>
+  <svg ref="qrcode"></svg>
+</template>
+
+<script>
+import QRCode from "qrcode";
+
+export default {
+  name: "HomePage",
+  methods: {
+    async generateQRCode() {
+      this.$refs.qrcode.innerHTML = await QRCode.toString("localhost:8080/about", {
+        errorCorrectionLevel: "M",
+        width: 150,
+        type: "svg",
+      });
+    }
+  }
+}
+</script>
